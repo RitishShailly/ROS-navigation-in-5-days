34bf0db136b1543dff1b793331f36ea0e5508f2d
Ritish Shaillly <rshailly@vt.edu>
2023-01-17 10:19:13 +0000

    Used AMCL package to pring current pose of a robot --- check catkin_pkg - get_pose

--- src/get_pose/scripts/get_pose_service.py
+++ src/get_pose/scripts/get_pose_service.py
@@ -2,3 +2,20 @@
 
 import sys
 import rospy
+
+from std_srvs.srv import Empty, EmptyResponse
+from geometry_msgs.msg import PoseWithCovarianceStamped
+
+
+def callback(msg):
+    print(msg.pose)
+
+
+def service_callback(request):
+    sub = rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, callback)
+    return EmptyResponse()
+
+
+rospy.init_node('get_pose_node')
+rospy.Service('/my_amcl_pose', Empty, service_callback)
+rospy.spin()
